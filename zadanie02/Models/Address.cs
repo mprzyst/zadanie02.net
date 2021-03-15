28a32ee75da8cbe3060f3af3f212c23c46c89f48
mprzyst <59484241+mprzyst@users.noreply.github.com>
2021-03-15 22:59:06 +0100

    Add project files.

---
+++ zadanie02/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zadanie02.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+
+        public string Street { get; set; }
+        
+        [StringLength(5, MinimumLength = 5), Required(ErrorMessage = "Enter valid postcode!")]
+        public string ZipCode { get; set; }
+        
+        [Required(ErrorMessage ="This field is required!")]
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ zadanie02/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using zadanie02.Models;
+
+namespace zadanie02.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "Magdalena";
+
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Privacy");
+        }
+
+    }
+}
